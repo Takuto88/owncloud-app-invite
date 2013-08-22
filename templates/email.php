5f2895e0b4204a79661134618ae1b7de5394f99f
Lennart Rosam <lennart@familie-rosam.de>
2013-08-23 01:00:43 +0200

    Print unescaped to avoid problems with umlauts in other languages

--- templates/email.php
+++ templates/email.php
@@ -1,9 +1,9 @@
 <?php
-p($l->t('Hello %s,', array($_['invitee'])) . "\n\n");
+print_unescaped($l->t('Hello %s,', array($_['invitee'])) . "\n\n");
 
-p($l->t('%s has invited you to join %s.' . " ", array($_['inviter'], $_['productname'])));
-p($l->t('You can accept this invitation by using the following link:' . "\n\n"));
+print_unescaped($l->t('%s has invited you to join %s.' . " ", array($_['inviter'], $_['productname'])));
+print_unescaped($l->t('You can accept this invitation by using the following link:' . "\n\n"));
 
 print_unescaped($_['link'] . "\n\n");
 
-p($l->t('If you have received this notification in error or if you do not wish to join, simply ignore this mail.'));
+print_unescaped($l->t('If you have received this notification in error or if you do not wish to join, simply ignore this mail.'));
